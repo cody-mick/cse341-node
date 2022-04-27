68b0a4651b2d738373325b6470546faa57db02d8
cody-mick <cmick@byui.edu>
2022-04-27 13:22:32 -0600

    ahhhhhhhhh

---
+++ routes/contacts.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+
+async function main() {
+    dotenv.config();
+
+    
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+    } catch(e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => 
+        console.log(` - ${db.name}`)
+    );
+}
+
+main().catch(console.error);
