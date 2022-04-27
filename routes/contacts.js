ff65a4ef4a5747410510f8dcb88b2dba5f1489fa
cody-mick <cmick@byui.edu>
2022-04-27 14:43:25 -0600

    sdgsfg

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,33 @@
 const {MongoClient} = require('mongodb');
 const dotenv = require('dotenv');
 
-async function main() {
-    dotenv.config();
+// async function main() {
+//     dotenv.config();
 
-    
+//     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}*@cluster0.hkyld.mongodb.net/database01?retryWrites=true&w=majority`
 
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
 
-    try {
-        await client.connect();
+//     try {
+//         await client.connect();
 
-        await listDatabases(client);
-    } catch(e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+//         await listDatabases(client);
+//     } catch(e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => 
-        console.log(` - ${db.name}`)
-    );
-}
-
-main().catch(console.error);
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => 
+//         console.log(` - ${db.name}`)
+//     );
+// }
+dotenv.config();
+console.log(`Username: ${process.env.DB_USER}`);
+console.log(`Password: ${process.env.DB_PASS}`);
+// main().catch(console.error);
