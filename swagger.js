ea5790d761d66bbbac9a4772be70df5eff2f8c47
cody-mick <cmick@byui.edu>
2022-05-13 20:55:17 -0600

    added api docs

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "My API",
+    description: "Contacts API",
+  },
+  host: "https://stormy-dawn-53463.herokuapp.com/",
+  schemes: ["https"],
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
